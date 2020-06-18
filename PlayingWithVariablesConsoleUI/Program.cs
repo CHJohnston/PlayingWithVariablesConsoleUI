6fa2a2bc2863cac9d9ef4c1788bdc44ef32ac203
CHJohnston <cdh.johnston@gmail.com>
2020-06-18 14:59:19 -0500

    TrueCoders MarkX11v1 Variables Exercise

--- PlayingWithVariablesConsoleUI/Program.cs
+++ PlayingWithVariablesConsoleUI/Program.cs
@@ -6,16 +6,17 @@ namespace PlayingWithVariablesConsoleUI
     {
         static void Main(string[] args)
         {
-            string myName = "Connie Johnston";
-            int age = 59;
+            string thisClass = "True Coders Mark XII v1";
+            int favNum = 23;
             char type = 'F';
             bool isMarried = true;
             double myHeight = 5.5;
-            decimal myMoneyGoal = 63500.25m;
+            decimal myMoneyGoal = 163500.25m;
 
             Console.WriteLine("True Coders C# Variables Exercise");
-            Console.WriteLine($"My Name is {myName}. My age is {age}"});
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"My class is {thisClass}. My favorite number is {favNum}");
+            Console.WriteLine($"I am a {type}. My married status is {isMarried}. I am {myHeight} ft. tall.");
+            Console.WriteLine($"I would like to earn ${myMoneyGoal}");
         }
     }
 }
