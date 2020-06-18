53a1e12070bb72dd5574e3f330a9b3378f252461
CHJohnston <cdh.johnston@gmail.com>
2020-06-18 14:17:07 -0500

    Add project files.

---
+++ PlayingWithVariablesConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Connie Johnston";
+            int age = 59;
+            char type = 'F';
+            bool isMarried = true;
+            double myHeight = 5.5;
+            decimal myMoneyGoal = 63500.25m;
+
+            Console.WriteLine("True Coders C# Variables Exercise");
+            Console.WriteLine($"My Name is {myName}. My age is {age}"});
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
